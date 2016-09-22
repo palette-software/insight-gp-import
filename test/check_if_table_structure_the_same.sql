77ba8b0d1b0532ca003001cf986ee89eade909b7
Levente Dologh <ldologh@gmail.com>
2016-09-22 09:39:08 +0200

    add missing function to the test sql file

--- test/check_if_table_structure_the_same.sql
+++ test/check_if_table_structure_the_same.sql
@@ -356,6 +356,38 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION py_load_tables_test.is_subpart_template_same(p_schema_name text, p_table_name text, p_subpart_cols text)
+RETURNS boolean AS
+$BODY$
+declare
+	v_subpart_cols text;
+	
+BEGIN
+
+	v_subpart_cols := '';
+	
+	select lower(string_agg(partitionname, ',')) into v_subpart_cols
+	from pg_partition_templates
+	where 
+		partitionname <> 'new_host' and
+		schemaname = p_schema_name and
+		tablename = p_table_name;
+		
+	if lower(p_subpart_cols) = v_subpart_cols then
+		return true;
+	else
+		return false;
+	end if;
+	
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE SECURITY INVOKER;
+
+
+
+
+
+
 
 
 
